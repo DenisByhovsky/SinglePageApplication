987e4493cc3405dab51fb7dca7ede1ce3e50b040
Denis_Byhovsky <denisbyh7@gmail.com>
2018-04-16 01:45:03 +0300

    Fixed bugs

--- src/main/java/com/epam/ajax/controller/SPAServlet.java
+++ src/main/java/com/epam/ajax/controller/SPAServlet.java
@@ -7,58 +7,45 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
 @WebServlet("/command")
 public class SPAServlet extends HttpServlet {
 
-    //LOG
+    //Add element
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        String column = req.getParameter("row");
-        String row = req.getParameter("col");
-        String command = req.getParameter("command");
-
-        if ((command != null) && (column != null)) {
-
-            // Add or remove items from the Cart
-            if ("addelem".equals(command)) {
-                // cart.addItem(item);
-
-            } else if ("delelem".equals(command)) {
-
-            }
-
-
-            resp.setContentType("text/html");
-            resp.setCharacterEncoding("UTF-8");
-            resp.getWriter().write("GOOD");
-        }
+        int column = Integer.parseInt(req.getParameter("row"));
+        int row = Integer.parseInt(req.getParameter("col"));
+        String name= req.getParameter("new-name");
+        new ExcelAction().sheetOperation(row,column,"add",name);
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write("good");
     }
 
-
+    //Delete element
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String column = req.getParameter("row");
-        String row = req.getParameter("col");
-        String command = req.getParameter("command");
-        new ExcelAction().deleteRow("Skill Matrix","9.xls", Integer.parseInt(row),command);
-
-        super.doPost(req, resp);
+        int column = Integer.parseInt(req.getParameter("row"));
+        int row = Integer.parseInt(req.getParameter("col"));
+        String name= "";
+        new ExcelAction().sheetOperation(row,column,"delete",name);
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write("good");;
     }
 
+    //Change element
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String column = req.getParameter("row");
-        String row = req.getParameter("col");
-        String command = req.getParameter("command");
-        super.doPut(req, resp);
+        String name = req.getParameter("name");
+        int row = Integer.parseInt(req.getParameter("row"));
+        int column = Integer.parseInt(req.getParameter("col"));
+        new ExcelAction().sheetOperation(row,column,"change",name);
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write("good");
     }
 
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
-    }
 }
