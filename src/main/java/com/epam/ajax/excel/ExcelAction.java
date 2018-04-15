3fffe85b49e2f22928305b39debea3d28941af5f
Denis_Byhovsky <denisbyh7@gmail.com>
2018-04-16 01:33:09 +0300

    Fixed bugs

--- src/main/java/com/epam/ajax/excel/ExcelAction.java
+++ src/main/java/com/epam/ajax/excel/ExcelAction.java
@@ -4,81 +4,70 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class ExcelAction {
 
-    public boolean deleteRow(String sheetName, String excelPath, int rowNo,String command) throws IOException {
+public class ExcelAction {
+    public static final String SH_NAME ="Skill Matrix";
+    public static final String EX_PATH ="9.xls";
+
+    public boolean sheetOperation( int rowNo, int cellNumb, String command,String name) throws IOException {
 
         HSSFWorkbook workbook = null;
         HSSFSheet sheet = null;
 
-
-            FileInputStream file = new FileInputStream(new File("9.xls"));
-            workbook = new HSSFWorkbook(file);
-            sheet = workbook.getSheet(sheetName);
-            if (sheet == null) {
-                return false;
-            }
-
-            switch (command) {
-                case "delete":
-                    int lastRowNum = sheet.getLastRowNum();
-                    if (rowNo >= 0 && rowNo < lastRowNum) {
-                        sheet.shiftRows(rowNo + 1, lastRowNum, -1);
-                    }
-                    if (rowNo == lastRowNum) {
-                        HSSFRow removingRow = sheet.getRow(rowNo);
-                        if (removingRow != null) {
-                            sheet.removeRow(removingRow);
-                        }
-                    }
+        FileInputStream file = new FileInputStream(new File(EX_PATH));
+        workbook = new HSSFWorkbook(file);
+        sheet = workbook.getSheet(SH_NAME);
 
 
-                case "add":
-
-                    if (rowNo >= 0 && rowNo < sheet.getLastRowNum()) {
-                        sheet.shiftRows(rowNo - 1, sheet.getLastRowNum(), +1);
-                    }
-                    if (rowNo == sheet.getLastRowNum()) {
-                        HSSFRow addRow = sheet.getRow(rowNo);
-                        if (addRow != null) {
-                            sheet.createRow(3);
-                            Row row = sheet.getRow(3);
-
-                            Cell cell = row.createCell(3);
-                            cell.setCellValue("90");
-
-
-                        }
-                    }
-
-            }
-                    file.close();
-                    FileOutputStream outFile = new FileOutputStream(new File("9.xls"));
-                    workbook.write(outFile);
-                    outFile.close();
-return  false;
-
-
+        if (sheet == null) {
+            return false;
         }
+        switch (command) {
+            case "delete":
+                int lastRowNum = sheet.getLastRowNum();
+                if (rowNo >= 0 && rowNo < lastRowNum) {
+                    sheet.shiftRows(rowNo + 1, lastRowNum, -1);
+                }
+                if (rowNo == lastRowNum) {
+                    HSSFRow removingRow = sheet.getRow(rowNo);
+                    if (removingRow != null) {
+                        sheet.removeRow(removingRow);
+                    }
+                }
+                break;
+            case "add":
 
+                if (rowNo >= 0 && rowNo < sheet.getLastRowNum()) {
+                    sheet.shiftRows(rowNo, sheet.getLastRowNum(),+1);
+                }
+                if (rowNo != sheet.getLastRowNum()) {
+                    HSSFRow addRow = sheet.getRow(rowNo);
 
+                    if (addRow != null) {
+                        sheet.createRow(rowNo);
+                        Cell cell = sheet.getRow(rowNo).createCell(cellNumb);
+                        sheet.getRow(rowNo).getCell(cellNumb);
+                        cell.setCellValue(name);
+                    }
+                }
+                break;
 
-    public  static  void main(String[] args) throws IOException {
-        ExcelAction excelAction = new ExcelAction();
-        excelAction.deleteRow("Skill Matrix","9.xls",3,"add");
+            case "change":
+                Cell cell = sheet.getRow(rowNo).createCell(cellNumb);
+                sheet.getRow(rowNo).getCell(cellNumb);
+                cell.setCellValue(name);
+                break;
+        }
+        file.close();
+        FileOutputStream outFile = new FileOutputStream(new File(EX_PATH));
+        workbook.write(outFile);
+        outFile.close();
+        return true;
     }
 
 }
-
-//int createNewRowAt = 9; //Add the new row between row 9 and 10
-
-//  sheet.shiftRows(createNewRowAt, sheet.getLastRowNum(), 1, true, false);
-// newRow = sheet.createRow(createNewRowAt);
-// newRow = sheet.getRow(createNewRowAt);
